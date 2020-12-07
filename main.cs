c380b2817fd432e698249d8b68f8df10b018647f
Oscar Michel Michel <osambelacolareta@gmail.com>
2020-12-07 03:48:58 +0000

    add code

--- main.cs
+++ main.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 /*
-Enum.- conjunto de constante con nombre
-Sirve para representar y manejar valores fijos (constantes) en un programa
+Enum.- conjunto de constantes con nombre
+Sirve para representar y manejar valores fijos (constantes) en un programa con valores integer no permite strings
 */
 public class Program
 {
@@ -15,11 +15,27 @@ public class Program
     Console.WriteLine(alergia);
 
     Bono Oscar = Bono.simple;
+
     double bonusAntonio = (double)Oscar;
     double salarioOscar = bonusAntonio + 54.60;
     Console.WriteLine(salarioOscar);
-    Empleado Francisco = new Empleado(Bono.supreme, 3000);
-    Console.WriteLine("El salario de Francisco más el bono \"supreme\" es: " + Francisco.getSalario());
+    
+		Empleado francisco = new Empleado(Bono.supreme, 3000);
+    Console.WriteLine("El salario de Francisco más el bono \"supreme\" es: " + francisco.getSalario());
+
+		Dias viernes = Dias.Vie;
+		Dias sabado = Dias.Sab;
+		Dias domingo = Dias.Dom;
+		Dias libre = Dias.Sab;
+		Console.WriteLine(sabado == libre);
+		Console.WriteLine(viernes == domingo);
+		//conocer el valor de un enum
+		Console.WriteLine((int)Dias.Mar);
+
+		Console.WriteLine("El mes de febrero es el número " + (int)Meses.Feb);
+
+		//si se reasigna un valor de cualquier posicion, el siguiente constante tomara el numero consiguiente. Por ejemplo Oct = 500, ver el resultado en consola de noviembre (501) 
+		Console.WriteLine((int)Meses.Nov);
   }
   //constantes en mayusculas por convencion
   //se asignara implicitamente un valor a cada constante empezando desde 0
@@ -41,4 +57,8 @@ public class Program
     }
 
   }
+
+	enum Dias{Lun, Mar, Mie, Jue, Vie, Sab, Dom};
+
+	enum Meses{Ene = 1,Feb, Mar, Abr, May, Jun, Jul, Ago, Set, Oct = 500, Nov, Dic};
 }
